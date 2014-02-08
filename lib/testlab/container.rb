221e45fe195010a1da893cce3cc727f58c968123
Zachary Patten <zachary@jovelabs.com>
2014-02-08 15:57:41 -0800

    container: add priority attribute

--- lib/testlab/container.rb
+++ lib/testlab/container.rb
@@ -144,6 +144,12 @@ class TestLab
     # Should we inherit a container?
     attribute   :inherit
 
+    # Execution priority; set the order in which the object should execute when
+    # performing parallel operations; a higher value priority equates to more
+    # precedence.  Objects with identical priority values will execute in
+    # parallel.
+    attribute   :priority,       :default => 0
+
 
     def initialize(*args)
       @ui = TestLab.ui
